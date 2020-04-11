d74f10bcaadeaf73c987c67b3b651ef44d367f17
Bruce <1556181289@qq.com>
2020-04-11 13:50:01 +0800

    添加首页模板

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
+
 @app.route('/')
 def index():
-    return 'Flask项目首页'
+    name = "Bruce"
+    movies = [
+        {'title':"大赢家",'year':"2020"},
+        {'title':"囧妈",'year':"2020"},
+        {'title':"疯狂外星人",'year':"2019"},
+        {'title':"战狼",'year':"2017"}
+    ]
+    return render_template("index.html",name=name,movies=movies)
